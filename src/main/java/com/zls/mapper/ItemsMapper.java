35bafcf8a875ca0bdb290e8fe562473962e7c885
zls <zls@zls>
2015-07-31 21:47:33 +0800

    按id查询[OK]

--- src/main/java/com/zls/mapper/ItemsMapper.java
+++ src/main/java/com/zls/mapper/ItemsMapper.java
@@ -1,11 +1,10 @@
 package com.zls.mapper;
 
-
 import org.springframework.stereotype.Component;
 
 import com.zls.po.Items;
 
-@Component("itemsMapper")
+@Component
 public interface ItemsMapper {
 	// 商品列表
 	public Items findItemsById(Integer id) throws Exception;
--- src/main/java/com/zls/service/impl/ItemsServiceImpl.java
+++ src/main/java/com/zls/service/impl/ItemsServiceImpl.java
@@ -1,11 +1,13 @@
 package com.zls.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.zls.mapper.ItemsMapper;
 import com.zls.po.Items;
 import com.zls.service.ItemsService;
 
+@Service
 public class ItemsServiceImpl implements ItemsService {
 	@Autowired
 	private ItemsMapper itemsMapper;
